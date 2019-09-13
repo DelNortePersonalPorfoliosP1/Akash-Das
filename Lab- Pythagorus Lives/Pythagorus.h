d3179ed87dccbd8ecf472643c91ef4c16e29766a
Akash Das <akash.das01@icloud.com>
2019-09-13 09:05:00 -0700

    My Pythagorus Code
    Lab Pythagorus

---
+++ Lab- Pythagorus Lives/Pythagorus.h
@@ -0,0 +1,14 @@
+//
+//  Pythagorus.h
+//  Lab- Pythagorus Lives
+//
+//  Created by Das, Akash on 9/11/19.
+//  Copyright © 2019 Das, Akash. All rights reserved.
+//
+
+#ifndef Pythagorus_h
+#define Pythagorus_h
+
+#include <stdio.h>
+int pythagorus(void) ;
+#endif /* Pythagorus_h */
